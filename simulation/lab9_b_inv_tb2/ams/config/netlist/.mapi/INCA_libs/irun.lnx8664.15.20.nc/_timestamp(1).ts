40395e213b3090f2ea415f246b801b3b7a6a3779
Joseph Crandall <josephcrandall@me.com>
2018-11-20 14:36:38 -0500

    hw3 update

---
+++ simulation/lab9_b_inv_tb2/ams/config/netlist/.mapi/INCA_libs/irun.lnx8664.15.20.nc/_timestamp(1).ts
@@ -0,0 +1,2 @@
+1540932104 /home/seas/jwcrandall/cadence/simulation/lab9_b_inv_tb2/ams/config/netlist/.mapi/dummyMapi.vams
+1540927575 /home/seas/jwcrandall/cadence/ECE_6240_Digial/lab9_b_inv_tb_vdriver/functional/verilog.v
